1917e6df5aacb7ee5643eef5a9a56f1cae5c6ba9
santiagonasar <mat.p.wasilewski@gmail.com>
2021-12-15 23:13:40 +0100

    add test for loading data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 
 from rem import utils
@@ -6,3 +8,17 @@ from rem import utils
 def test_load_data() -> None:
     loaded_data = utils.load_data('warszawa-sprzedaz.csv', "resources")
     assert isinstance(loaded_data, pd.DataFrame)
+
+
+def test_save_data() -> None:
+    test_data = pd.DataFrame({"test": [1, 2, 3]})
+
+    path = os.sep.join(["resources", "test-save-and-load.csv"])
+    os.remove(path)
+    utils.save_data(test_data, 'test-save-and-load.csv', "resources")
+    data = utils.load_data('test-save-and-load.csv', "resources")
+
+    os.remove(path)
+
+    assert len(data) == 3
+    assert data.loc["test"] == [1, 2, 3]
