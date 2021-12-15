745143b5618553e1097f4231d30faa629ae4c61a
santiagonasar <mat.p.wasilewski@gmail.com>
2021-12-15 22:45:08 +0100

    remove comments, black reformatting

--- rem/main.py
+++ rem/main.py
@@ -4,10 +4,14 @@ import sys
 
 import validators
 
-logging.basicConfig(filename='otodom.log',
-                    level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(threadName)s -  %('
-                           'levelname)s - %(message)s')
+from rem import otodom
+
+logging.basicConfig(
+    filename='otodom.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(threadName)s -  %('
+    'levelname)s - %(message)s',
+)
 
 
 # setup argparser with: propertytype, rentaltype, city, savephotos
@@ -17,47 +21,25 @@ def parse_args(args):
     parser = argparse.ArgumentParser(description='Provide input scrapper args')
 
     # @TODO: Add url validator
-    parser.add_argument('--url', nargs="?", required=True,
-                        help='Enter otodom search url')
-
-    parser.add_argument('--page_limit', nargs="?", required=False,
-                        type=int, default=1,
-                        help='Enter otodom search url')
-
+    parser.add_argument(
+        '--url', nargs="?", required=True, help='Enter otodom search url'
+    )
+    parser.add_argument(
+        '--page_limit',
+        nargs="?",
+        required=False,
+        type=int,
+        default=1,
+        help='Enter otodom search url',
+    )
 
     return parser.parse_args(args)
 
 
-# create all necessary urls and dirs based on categories
-# return dictionary {page_url : path_to_saving_data}
-def get_urls_dirs(args):
-    urls_data = {}
-
-    for propertytype in args.propertytype:
-        for rentaltype in args.rentaltype:
-            for city in args.city:
-                pass
-                # @FIXME
-                # if rentaltype == 'selling' and propertytype == 'room':
-                #     continue
-                # main_url = ParseResult(scheme='https', netloc=hostURL,
-                #                         path=(f'{typesPL.get(rentaltype)}/'+
-                #                             f'{typesPL.get(propertytype)}/'+
-                #                             f'{city.lower()}/'), params='',
-                #                         query=(f'nrAdsPerPage={offersPerPage}'),
-                #                         fragment='').geturl()
-                # json_path = f'{rentaltype}-{propertytype}-{city.lower()}'
-                # urls_data[main_url] = json_path
-    return urls_data
-
-
 def main():
-    parser = parse_args(sys.argv[1:])
-    args = get_args()
-
-    urls_dirs = get_urls_dirs(args)
-
-    # proceed(urls_dirs, args.savephotos)
+    args = parse_args(sys.argv[1:])
+    logging.info(f"Starting scrapping of {args.url}...")
+    scrapped_data = otodom.otodom_scrap(args.url, args.page_limit)
 
 
 if __name__ == "__main__":
