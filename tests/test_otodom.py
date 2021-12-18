ae94d26b20bf786ec44781005b48199ae7042c4c
OktawiaM <oktawiamiluch28@gmail.com>
2021-12-18 21:15:11 +0100

    test get_parking_space

--- tests/test_otodom.py
+++ tests/test_otodom.py
@@ -241,8 +241,8 @@ def test_resolve_monthly_fee_5(otodom_instance) -> None:
 def test_get_listing_url(otodom_instance, listing) -> None:
     listing_url = otodom_instance.get_listing_url(listing)
     assert (
-        listing_url["url"]
-        == "https://www.otodom.pl/pl/oferta/mieszkanie-w-kamienicy-w-srodmiesciu-ID4dG6i.html"
+            listing_url["url"]
+            == "https://www.otodom.pl/pl/oferta/mieszkanie-w-kamienicy-w-srodmiesciu-ID4dG6i.html"
     )
 
 
@@ -287,10 +287,12 @@ def test_seller_type(otodom_instance, listing) -> None:
     seller_type = otodom_instance.get_seller_type(listing)
     assert seller_type == {"seller_type": "agency"}
 
+
 @pytest.mark.skip
 def test_extract_long_lat_via_address(otodom_gcp_instance, listing) -> None:
     coordinates = otodom_gcp_instance.extract_long_lat_via_address(listing)
     assert coordinates == {"latitude": 52.2098433, "longitude": 21.028336}
+
 
 @pytest.mark.skip
 def test_get_transit_time_distance(otodom_gcp_instance, listing) -> None:
@@ -302,67 +304,76 @@ def test_get_transit_time_distance(otodom_gcp_instance, listing) -> None:
         "commuting_time_min": "42 mins",
     }
 
+
 def test_resolve_additional_features(otodom_instance, listing) -> None:
     additional_features_list = otodom_instance.resolve_additional_features(listing)
-    assert additional_features_list == ['internet', 'system alarmowy', 'drzwi / okna antywłamaniowe', 'teren zamknięty', 'domofon / wideofon', 'klimatyzacja', 'balkon', 'piwnica', 'garaż/miejsce parkingowe']
+    assert additional_features_list == ['internet', 'system alarmowy', 'drzwi / okna antywłamaniowe', 'teren zamknięty',
+                                        'domofon / wideofon', 'klimatyzacja', 'balkon', 'piwnica',
+                                        'garaż/miejsce parkingowe']
+
+
+def test_get_parking_space(otodom_instance, listing) -> None:
+    parking_space = otodom_instance.get_parking_space(listing)
+    assert parking_space == {"parking": 1, "garage": 1}
+
 
 def test_get_promoted_listing_urls_for_search_page(
-    otodom_instance, search_soup
+        otodom_instance, search_soup
 ) -> None:
     promoted_urls = otodom_instance.get_promoted_listing_urls_for_page(
         search_soup
     )
     assert len(promoted_urls) == 3
     assert (
-        promoted_urls[0] == "https://www.otodom.pl/pl/oferta/nowa"
-        "-kawalerka-odbior-kluczy-1q2022-ochota-wloch"
-        "-ID4blGn"
+            promoted_urls[0] == "https://www.otodom.pl/pl/oferta/nowa"
+                                "-kawalerka-odbior-kluczy-1q2022-ochota-wloch"
+                                "-ID4blGn"
     )
     assert (
-        promoted_urls[1] == "https://www.otodom.pl/pl/oferta/apartament"
-        "-130-m-w-babka-tower-ID4ehmq"
+            promoted_urls[1] == "https://www.otodom.pl/pl/oferta/apartament"
+                                "-130-m-w-babka-tower-ID4ehmq"
     )
     assert (
-        promoted_urls[2] == "https://www.otodom.pl/pl/oferta/penthouse-na"
-        "-marymonckiej-ID4ehkP"
+            promoted_urls[2] == "https://www.otodom.pl/pl/oferta/penthouse-na"
+                                "-marymonckiej-ID4ehkP"
     )
 
 
 def test_get_standard_listintg_urls_for_search_page(
-    otodom_instance, search_soup
+        otodom_instance, search_soup
 ) -> None:
     standard_urls = otodom_instance.get_standard_listing_urls_for_page(
         search_soup
     )
     assert len(standard_urls) == 36
     assert (
-        standard_urls[0] == "https://www.otodom.pl/pl/oferta/kawalerka"
-        "-warszawa-ul-fundamentowa-ID47bq4"
+            standard_urls[0] == "https://www.otodom.pl/pl/oferta/kawalerka"
+                                "-warszawa-ul-fundamentowa-ID47bq4"
     )
     assert (
-        standard_urls[1] == "https://www.otodom.pl/pl/oferta/mieszkanie"
-        "-dla-rodziny-przy-parku-szczesliwickim"
-        "-ID4dVV3"
+            standard_urls[1] == "https://www.otodom.pl/pl/oferta/mieszkanie"
+                                "-dla-rodziny-przy-parku-szczesliwickim"
+                                "-ID4dVV3"
     )
     assert (
-        standard_urls[-2] == "https://www.otodom.pl/pl/oferta/dwupokojowe"
-        "-nowe-i-do-odbioru-ID4ebFQ"
+            standard_urls[-2] == "https://www.otodom.pl/pl/oferta/dwupokojowe"
+                                 "-nowe-i-do-odbioru-ID4ebFQ"
     )
     assert (
-        standard_urls[-1] == "https://www.otodom.pl/pl/oferta/z-tarasem"
-        "-18-52m2-10min-do-centrum-blisko-skm-ID4enyi"
+            standard_urls[-1] == "https://www.otodom.pl/pl/oferta/z-tarasem"
+                                 "-18-52m2-10min-do-centrum-blisko-skm-ID4enyi"
     )
 
 
 def test_get_all_listings_for_search_page(
-    otodom_instance, search_soup
+        otodom_instance, search_soup
 ) -> None:
     urls = otodom_instance.get_all_relevant_listing_urls_for_page(search_soup)
     assert len(urls) == 39
 
 
 def test_get_empty_list_of_urls_for_empty_page(
-    otodom_instance, empty_search_soup
+        otodom_instance, empty_search_soup
 ) -> None:
     urls = otodom_instance.get_all_relevant_listing_urls_for_page(
         empty_search_soup
@@ -377,18 +388,18 @@ def test_url_generator(otodom_instance):
     url_generator = otodom_instance.url_generator()
 
     assert (
-        next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
-        "/mieszkanie/warszawa?page=1&limit=36"
+            next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
+                                   "/mieszkanie/warszawa?page=1&limit=36"
     )
     assert (
-        next(url_generator)
-        == "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa"
-        "?page=2&limit=36"
+            next(url_generator)
+            == "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa"
+               "?page=2&limit=36"
     )
     assert (
-        next(url_generator)
-        == "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa"
-        "?page=3&limit=36"
+            next(url_generator)
+            == "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa"
+               "?page=3&limit=36"
     )
 
 
@@ -397,16 +408,16 @@ def test_url_generator_with_query_parameters(otodom_instance):
 
     url_generator = otodom_instance.url_generator()
     assert (
-        next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
-        "/mieszkanie/warszawa?page=1&limit=72"
+            next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
+                                   "/mieszkanie/warszawa?page=1&limit=72"
     )
     assert (
-        next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
-        "/mieszkanie/warszawa?page=2&limit=72"
+            next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
+                                   "/mieszkanie/warszawa?page=2&limit=72"
     )
     assert (
-        next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
-        "/mieszkanie/warszawa?page=3&limit=72"
+            next(url_generator) == "https://www.otodom.pl/pl/oferty/sprzedaz"
+                                   "/mieszkanie/warszawa?page=3&limit=72"
     )
 
 
@@ -424,7 +435,7 @@ def test_get_data_from_listing(otodom_instance, listing) -> None:
 
 
 def test_update_listing_data(
-    otodom_instance, listing: BeautifulSoup, alternative_listing: BeautifulSoup
+        otodom_instance, listing: BeautifulSoup, alternative_listing: BeautifulSoup
 ) -> None:
     listing_soups = [listing, alternative_listing]
     otodom_instance.process_listing_soups(listing_soups)
@@ -452,8 +463,8 @@ def test_main_page_not_scraped(otodom_instance, search_soup) -> None:
         search_soup
     )
     assert (
-        "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa"
-        not in relevant_listings
+            "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa"
+            not in relevant_listings
     )
 
 
